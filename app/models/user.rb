d764f1131356d00e26c2676f3a696f58a9582c09
Joshua Doud <bigjj3393@yahoo.com>
2023-11-17 12:13:51 -0800

    mistype

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
     has_many :impounds
     has_many :animals, through: :impounds
-    has_many :clients. through: :impounds
+    has_many :clients, through: :impounds
 
     validates :user_name, presence:true, uniqueness: true
     validates :password, length: {in: 6..20}
