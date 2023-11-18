fb41a7f5efad625ae06d1e8ea03a78f9a1c83125
Joshua Doud <bigjj3393@yahoo.com>
2023-11-18 12:36:56 -0800

    fixed typo in animals controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,5 +1,5 @@
 class AnimalsController < ApplicationController
-    skip_before_action :authorized, only:index
+    skip_before_action :authorized, only: :index
 
     def index 
         render json: Animal.all
