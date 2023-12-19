29a3b8729aad9718990ad6eb9c4594c0806f154f
Joshua <bigjj3393@yahoo.com>
2023-12-18 16:33:36 -0800

    fixed home screen

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,7 +1,13 @@
 
 function Home () {
     return(
-        <h1>Home</h1>
+        <div id="home-page">
+            <h1>Welcome to kennel Mate 2.0</h1>
+            <br/>
+            <img id="animals-img" src="https://images.ctfassets.net/rt5zmd3ipxai/5nZeJ7jOTRTKZDw10Wctv5/f114c3fc1f7c89fe7a4bfcdbf3323008/NVA-City-Dog-Cat-Sleeping-Grass.jpeg?fit=fill&fm=webp&h=567&w=1485&q=75,%20https://images.ctfassets.net/rt5zmd3ipxai/5nZeJ7jOTRTKZDw10Wctv5/f114c3fc1f7c89fe7a4bfcdbf3323008/NVA-City-Dog-Cat-Sleeping-Grass.jpeg?fit=fill&fm=webp&h=1134&w=2970&q=75"/>
+            <br />
+            <h2>Please explore all of our options. This application is meant for kennel employees to help keep track of clients and animals. We also have an impound screen where you can impound an animal into the database and make a selection for where the animal is staying in your kennels. This also features a virtual kennel where you can quickly view the animals in each kennel. </h2>
+        </div>
     )
 }
 
